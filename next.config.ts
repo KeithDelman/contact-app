d38ce707a946375db729919c0142e4d5c1368b68
KeithDelman <142185213+KeithDelman@users.noreply.github.com>
2026-02-14 10:44:01 +0800

    Initial commit - Contact app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // iba mo pang config options kung meron
+  async headers() {
+    return [
+      {
+        source: '/:path*', // apply sa lahat ng routes
+        headers: [
+          { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
+          { key: 'Content-Security-Policy', value: "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:;" },
+          { key: 'X-XSS-Protection', value: '1; mode=block' },
+          { key: 'X-Frame-Options', value: 'DENY' },
+          
+          
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
