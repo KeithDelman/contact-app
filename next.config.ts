7682525a0c7d6ba2633d2aa05adc5c45c22811f7
KeithDelman <142185213+KeithDelman@users.noreply.github.com>
2026-02-14 10:55:45 +0800

    Update contact form / add styles / update headers

--- next.config.ts
+++ next.config.ts
@@ -7,10 +7,7 @@ const nextConfig: NextConfig = {
       {
         source: '/:path*', // apply sa lahat ng routes
         headers: [
-          { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
-          { key: 'Content-Security-Policy', value: "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:;" },
-          { key: 'X-XSS-Protection', value: '1; mode=block' },
-          { key: 'X-Frame-Options', value: 'DENY' },
+          
           
           
         ],
